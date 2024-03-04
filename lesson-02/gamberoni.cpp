cd38ae1d1fb788c33cb272c58b1fbe1888aae04a
Antonio Scardace <33966051+antonioscardace@users.noreply.github.com>
2024-03-04 21:50:19 +0100

    add: solution for the "gamberoni" task

---
+++ lesson-02/gamberoni.cpp
@@ -0,0 +1,30 @@
+#include <fstream>
+using namespace std;
+
+int main() {
+    ifstream in("input.txt");
+    ofstream out("output.txt");
+
+    for (int task = 0; task < 100; task++) {
+
+        int N;
+        in >> N;
+
+        double result = 0;
+        double minimo = 100;
+        for (int i = 0; i < N; i++) {
+            
+            double tmp;
+            in >> tmp;
+            
+            if (tmp < minimo) minimo = tmp;
+            else if(tmp - minimo > result) result = tmp - minimo;
+        }
+
+        out << result << endl;
+    }
+
+    in.close();
+    out.close();
+    return 0;
+}
